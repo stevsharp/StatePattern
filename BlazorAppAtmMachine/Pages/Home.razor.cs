26af15f5aec530135d49f7ee3c171ee5a4760f34
SPYROS\Spyros <sponaris@gmail.com>
2025-07-03 16:37:27 +0300

    ..

--- BlazorAppAtmMachine/Pages/Home.razor.cs
+++ BlazorAppAtmMachine/Pages/Home.razor.cs
@@ -22,69 +22,69 @@ public partial class Home
     {
         AtmMachine.OnStateChanged -= StateHasChanged;
     }
-
-    //private string statusMessage;
-
-    //private ATM _atm;
-
-    //private bool isInsertCardDisabled;
-    //private bool isEjectCardDisabled;
-    //private bool isEnterPinDisabled;
-    //private bool isRequestCashDisabled;
-    //private int enteredPin = 1234;
-
-    //private void SubmitPin()
-    //{
-    //    AtmMachine.EnterPIN(enteredPin);
-    //}
-
-    //protected override void OnInitialized()
-    //{
-    //    _atm = new ATM(500);            // Initialize ATM with some cash
-    //    AtmMachine.OnStateChanged += StateHasChanged;
-    //    UpdateStatus();
-    //}
-
-    //private void InsertCard()
-    //{
-    //    _atm.CurrentState.InsertCard(_atm);
-
-    //    UpdateStatus();
-    //}
-
-    //private void EjectCard()
-    //{
-    //    _atm.CurrentState.EjectCard(_atm);
-
-    //    UpdateStatus();
-    //}
-
-    //private void EnterPin()
-    //{
-    //    _atm.CurrentState.InsertPin(_atm, 1234); // Example PIN
-
-    //    UpdateStatus();
-    //}
-
-    //private void RequestCash()
-    //{
-    //    _atm.CurrentState.RequestCash(_atm, 100); // Example cash amount
-
-    //    UpdateStatus();
-    //}
-
-    //private void UpdateStatus()
-    //{
-    //    statusMessage = $"ATM current state: {_atm.CurrentState.GetType().Name} - Money {_atm.CashInMachine}";
-
-    //    isInsertCardDisabled = _atm.CurrentState is HasCardState || _atm.CurrentState is HasCorrectPinState || _atm.CurrentState is NoCashState;
-    //    isEjectCardDisabled = _atm.CurrentState is NoCardState || _atm.CurrentState is NoCashState;
-    //    isEnterPinDisabled = _atm.CurrentState is NoCardState || _atm.CurrentState is HasCorrectPinState || _atm.CurrentState is NoCashState;
-    //    isRequestCashDisabled = _atm.CurrentState is NoCardState || _atm.CurrentState is HasCardState || _atm.CurrentState is NoCashState || !_atm.CorrectPinEntered;
-    //}
-
-    //public void Dispose()
-    //{
-    //    AtmMachine.OnStateChanged -= StateHasChanged;
-    //}
 }
+
+//private string statusMessage;
+
+//private ATM _atm;
+
+//private bool isInsertCardDisabled;
+//private bool isEjectCardDisabled;
+//private bool isEnterPinDisabled;
+//private bool isRequestCashDisabled;
+//private int enteredPin = 1234;
+
+//private void SubmitPin()
+//{
+//    AtmMachine.EnterPIN(enteredPin);
+//}
+
+//protected override void OnInitialized()
+//{
+//    _atm = new ATM(500);            // Initialize ATM with some cash
+//    AtmMachine.OnStateChanged += StateHasChanged;
+//    UpdateStatus();
+//}
+
+//private void InsertCard()
+//{
+//    _atm.CurrentState.InsertCard(_atm);
+
+//    UpdateStatus();
+//}
+
+//private void EjectCard()
+//{
+//    _atm.CurrentState.EjectCard(_atm);
+
+//    UpdateStatus();
+//}
+
+//private void EnterPin()
+//{
+//    _atm.CurrentState.InsertPin(_atm, 1234); // Example PIN
+
+//    UpdateStatus();
+//}
+
+//private void RequestCash()
+//{
+//    _atm.CurrentState.RequestCash(_atm, 100); // Example cash amount
+
+//    UpdateStatus();
+//}
+
+//private void UpdateStatus()
+//{
+//    statusMessage = $"ATM current state: {_atm.CurrentState.GetType().Name} - Money {_atm.CashInMachine}";
+
+//    isInsertCardDisabled = _atm.CurrentState is HasCardState || _atm.CurrentState is HasCorrectPinState || _atm.CurrentState is NoCashState;
+//    isEjectCardDisabled = _atm.CurrentState is NoCardState || _atm.CurrentState is NoCashState;
+//    isEnterPinDisabled = _atm.CurrentState is NoCardState || _atm.CurrentState is HasCorrectPinState || _atm.CurrentState is NoCashState;
+//    isRequestCashDisabled = _atm.CurrentState is NoCardState || _atm.CurrentState is HasCardState || _atm.CurrentState is NoCashState || !_atm.CorrectPinEntered;
+//}
+
+//public void Dispose()
+//{
+//    AtmMachine.OnStateChanged -= StateHasChanged;
+//}
